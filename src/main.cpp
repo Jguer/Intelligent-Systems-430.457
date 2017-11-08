dd695068f098cd93bb64c736ad577d87fd66f4a9
Jguer <me@jguer.space>
2017-11-08 22:45:27 +0900

    Path to 13/20

--- src/main.cpp
+++ src/main.cpp
@@ -289,8 +289,8 @@ void set_waypoints() {
   waypoint_candid[3].y = 7.0;
   waypoint_candid[3].th = 0.0;
 
-  int order[] = {0, 1, 2, 3};
-  int order_size = 4;
+  int order[] = {3, 1, 2, 3};
+  int order_size = 3;
 
   for (int i = 0; i < order_size; i++) {
     waypoints.push_back(waypoint_candid[order[i]]);
