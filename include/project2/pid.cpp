ca51a27d4c3cfe496fdecbf12f4bbfecb9cf9270
Jguer <me@jguer.space>
2017-11-08 22:24:46 +0900

    Path to 13/20

--- include/project2/pid.cpp
+++ include/project2/pid.cpp
@@ -13,7 +13,7 @@ PID::PID(double Kp_in, double Ki_in, double Kd_in) {
   error_diff = 0;
   error_sum = 0;
 
-  freq = 0.1;
+  freq = 1 / 60;
 }
 
 PID::~PID() {}
@@ -28,16 +28,15 @@ float PID::get_control(point car_pose, traj goal_pose) {
   // Angle
   des_angle =
       atan2(goal_pose.y - car_pose.y, goal_pose.x - car_pose.x) - car_pose.th;
-  printf("Atan, Theta: %0.2f %0.2f %0.2f\n", des_angle, car_pose.th, error_sum);
 
   // Updating Error
   error_diff = des_angle - error;
   error = des_angle;
-  error_sum += error;
+  error_sum += error * freq;
 
   // Calculating P I D
   p = Kp * error;
-  i = Ki * freq * error_sum;
+  i = Ki * error_sum;
   d = Kd * error_diff / freq;
 
   // Control Value
