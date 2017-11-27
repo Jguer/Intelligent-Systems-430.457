8aba03356289b0a32c721b36060f957e30b7e920
Jguer <me@jguer.space>
2017-11-27 16:40:30 +0900

    Turned on, ze stuff

--- include/project2/rrtTree.cpp
+++ include/project2/rrtTree.cpp
@@ -294,11 +294,11 @@ int rrtTree::nearestNeighbor(point x_rand, double MaxStep) {
 
     rel_th = atan2((x_rand.y - x_near.y), (x_rand.x - x_near.x));
 
-    /* if (rel_th >= max_th || rel_th <= min_th) { */
-    /*   std::cout << "Fell out (" << rel_th << ") Point: "; */
-    /*   x_near.print(); */
-    /*   continue; */
-    /* } */
+    if (rel_th >= max_th || rel_th <= min_th) {
+      std::cout << "Fell out (" << rel_th << ") Point: ";
+      x_near.print();
+      continue;
+    }
 
     if (dist_to_rand < distance_min) {
       distance_min = dist_to_rand;
