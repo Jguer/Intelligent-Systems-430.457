09da1e795cdd7221acd87590a593fcbc48e93826
Jguer <me@jguer.space>
2017-11-27 21:52:35 +0900

    Finished Refactoring

--- include/project2/traj.cpp
+++ include/project2/traj.cpp
@@ -27,15 +27,6 @@ void traj::print() {
   printf("X: %.2f Y:%.2f TH:%.2f D:%.2f A:%.2f\n", x, y, th, d, alpha);
 }
 
-point traj::convertToPoint() {
-  point p_new;
-  p_new.x = x;
-  p_new.y = y;
-  p_new.th = th;
-
-  return p_new;
-}
-
 void traj::set(double new_x, double new_y, double new_th, double new_alpha,
                double new_d) {
   x = new_x;
