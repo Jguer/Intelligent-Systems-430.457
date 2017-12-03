456f8c1aa3b458cc001f93c923cccc845bfb7e27
Jguer <me@jguer.space>
2017-12-03 17:18:38 +0900

    Project 4 Skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 // state definition
 #define INIT 0
-#define RUNNING 1
-#define MAPPING 2
-#define PATH_PLANNING 3
+#define PATH_PLANNING 1
+#define RUNNING 2
 #define FINISH -1
 
 #include <ackermann_msgs/AckermannDriveStamped.h>
-#include <cmath>
-#include <gazebo_msgs/DeleteModel.h>
-#include <gazebo_msgs/ModelStates.h>
-#include <gazebo_msgs/SetModelState.h>
-#include <gazebo_msgs/SpawnModel.h>
-#include <project2/pid.h>
-#include <project2/rrtTree.h>
+#include <math.h>
+#include <project4/pid.h>
+#include <project4/rrtTree.h>
 #include <pwd.h>
 #include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Time.h>
 #include <tf/transform_datatypes.h>
 #include <unistd.h>
+
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 
 // map spec
 cv::Mat map;
@@ -31,27 +30,20 @@ double world_y_min;
 double world_y_max;
 
 // parameters you should adjust : K, margin, MaxStep
-int margin = 17; // 17
-int K = 20000; // 6000
-double MaxStep = 2.5; // 2.5
+int margin = 15;
+int K = 500;
+double MaxStep = 2;
+int waypoint_margin = 22;
 
-// way points
+// Vectoring
 std::vector<point> waypoints;
-
-// path
-// std::vector<point> path_RRT;
 std::vector<traj> path_RRT;
 
-// control
-// std::vector<control> control_RRT;
-
-// robot
+// Robot Parameters
 point robot_pose;
 ackermann_msgs::AckermannDriveStamped cmd;
 double speed;
 double angle;
-//
-// at full joystick depression you'll go this fast
 double max_speed = 2.00;
 double max_turn = 60.0 * M_PI / 180.0;
 
@@ -59,10 +51,10 @@ double max_turn = 60.0 * M_PI / 180.0;
 int state;
 
 // function definition
+void setcmdvel(double v, double w);
+void callback_state(geometry_msgs::PoseWithCovarianceStampedConstPtr msgs);
 void set_waypoints();
 void generate_path_RRT();
-void callback_state(gazebo_msgs::ModelStatesConstPtr msgs);
-void setcmdvel(double v, double w);
 
 void spawn_robot(ros::ServiceClient gazebo_set) {
   geometry_msgs::Pose model_pose;
@@ -96,122 +88,71 @@ void spawn_robot(ros::ServiceClient gazebo_set) {
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "rrt_main");
+  ros::init(argc, argv, "slam_main");
   ros::NodeHandle n;
 
   // Initialize topics
-  ros::Subscriber gazebo_pose_sub =
-      n.subscribe("/gazebo/model_states", 100, callback_state);
   ros::Publisher cmd_vel_pub =
       n.advertise<ackermann_msgs::AckermannDriveStamped>(
-          "/vesc/low_level/ackermann_cmd_mux/output", 100);
-  ros::ServiceClient gazebo_spawn =
-      n.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
-  ros::ServiceClient gazebo_set =
-      n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
-  ros::ServiceClient deleteClient =
-      n.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
-  printf("Initialize topics\n");
+          "/vesc/high_level/ackermann_cmd_mux/input/nav_0", 1);
 
-  // Load Map
-
-  char *user = getpwuid(getuid())->pw_name;
-  map = cv::imread(
-      (std::string("/home/") + std::string(user) +
-       std::string("/catkin_ws/src/project2/src/ground_truth_map.pgm"))
-          .c_str(),
-      CV_LOAD_IMAGE_GRAYSCALE);
-
-  map_y_range = map.cols;
-  map_x_range = map.rows;
-  map_origin_x = map_x_range / 2.0 - 0.5;
-  map_origin_y = map_y_range / 2.0 - 0.5;
-  world_x_min = -10.0;
-  world_x_max = 10.0;
-  world_y_min = -10.0;
-  world_y_max = 10.0;
-  res = 0.05;
-  printf("Load map\n");
-
-  if (!map.data) {
-    printf("Could not open or find the image\n");
-    return -1;
-  }
-
-  // Set Way Points
-  set_waypoints();
-  printf("Set way points\n");
-
-  // RRT
-  generate_path_RRT();
-  printf("Generate RRT\n");
+  ros::Subscriber gazebo_pose_sub =
+      n.subscribe("/amcl_pose", 100, callback_state);
+  std::cout << "Initialized Topics" << std::endl;
 
   // FSM
   state = INIT;
   bool running = true;
-  int look_ahead_idx;
   PID *pid_ctrl = new PID(0.6, 0.3, 0.0);
   ros::Rate control_rate(60);
 
   while (running) {
     switch (state) {
     case INIT: {
-      look_ahead_idx = 0;
       srand(time(NULL));
-      std::cout << "Path Size: " << path_RRT.size() << std::endl;
 
-      // visualize path
-      for (int i = 0; i < path_RRT.size(); i++) {
-        gazebo_msgs::SpawnModel model;
-        model.request.model_xml =
-            std::string("<robot name=\"simple_ball\">") +
-            std::string("<static>true</static>") +
-            std::string("<link name=\"ball\">") + std::string("<inertial>") +
-            std::string("<mass value=\"1.0\" />") +
-            std::string("<origin xyz=\"0 0 0\" />") +
-            std::string("<inertia  ixx=\"1.0\" ixy=\"1.0\"  ixz=\"1.0\"  "
-                        "iyy=\"1.0\"  iyz=\"1.0\"  izz=\"1.0\" />") +
-            std::string("</inertial>") + std::string("<visual>") +
-            std::string("<origin xyz=\"0 0 0\" rpy=\"0 0 0\" />") +
-            std::string("<geometry>") +
-            std::string("<sphere radius=\"0.09\"/>") +
-            std::string("</geometry>") + std::string("</visual>") +
-            std::string("<collision>") +
-            std::string("<origin xyz=\"0 0 0\" rpy=\"0 0 0\" />") +
-            std::string("<geometry>") +
-            std::string("<sphere radius=\"0.09\"/>") +
-            std::string("</geometry>") + std::string("</collision>") +
-            std::string("</link>") +
-            std::string("<gazebo reference=\"ball\">") +
-            std::string("<mu1>10</mu1>") + std::string("<mu2>10</mu2>") +
-            std::string("<material>Gazebo/Blue</material>") +
-            std::string("<turnGravityOff>true</turnGravityOff>") +
-            std::string("</gazebo>") + std::string("</robot>");
+      // Load Map
+      char *user = getpwuid(getuid())->pw_name;
+      cv::Mat map_org =
+          cv::imread((std::string("/home/") + std::string(user) +
+                      std::string("/catkin_ws/src/project4/src/slam_map.pgm"))
+                         .c_str(),
+                     CV_LOAD_IMAGE_GRAYSCALE);
 
-        model.request.model_name = std::to_string(i);
-        model.request.reference_frame = "world";
-        model.request.initial_pose.position.x = path_RRT[i].x;
-        model.request.initial_pose.position.y = path_RRT[i].y;
-        model.request.initial_pose.position.z = 0.7;
-        model.request.initial_pose.orientation.w = 0.0;
-        model.request.initial_pose.orientation.x = 0.0;
-        model.request.initial_pose.orientation.y = 0.0;
-        model.request.initial_pose.orientation.z = 0.0;
-        gazebo_spawn.call(model);
-        ros::spinOnce();
+      cv::transpose(map_org, map);
+      cv::flip(map, map, 1);
+
+      map_y_range = map.cols;
+      map_x_range = map.rows;
+      map_origin_x = map_x_range / 2.0 - 0.5;
+      map_origin_y = map_y_range / 2.0 - 0.5;
+      world_x_min = -4.5;
+      world_x_max = 4.5;
+      world_y_min = -13.5;
+      world_y_max = 13.5;
+      res = 0.05;
+      std::cout << "Loaded Map\n" << std::endl;
+
+      if (!map.data) {
+        // Check for invalid input
+        printf("Could not open or find the image\n");
+        return -1;
       }
-      std::cout << "Spawn Path" << std::endl;
+      state = PATH_PLANNING;
+    } break;
+    case PATH_PLANNING: {
+      // Set Way Points
+      set_waypoints();
+      printf("Set way points\n");
 
-      // initialize robot position
-      spawn_robot(gazebo_set);
+      // RRT
+      generate_path_RRT();
+      printf("Generate RRT\n");
+
       ros::spinOnce();
       ros::Rate(0.33).sleep();
 
-      std::cout << "Initialize Robot" << std::endl;
-      std::cout << "Path List" << std::endl;
-      for (int qq = 0; qq < path_RRT.size(); qq++) {
-        path_RRT[qq].print();
-      }
+      printf("Initialize ROBOT\n");
       state = RUNNING;
     } break;
     case RUNNING: {
@@ -225,7 +166,8 @@ int main(int argc, char **argv) {
                                                   path_RRT[look_ahead_idx].y);
       if (dist_to_target <= 0.4) {
         std::cout << "New destination" << std::endl;
-        printf("x, y : %.2f, %.2f \n", path_RRT[look_ahead_idx].x, path_RRT[look_ahead_idx].y);
+        printf("x, y : %.2f, %.2f \n", path_RRT[look_ahead_idx].x,
+               path_RRT[look_ahead_idx].y);
         /* Project: Ball Removal (crash)
         gazebo_msgs::DeleteModelRequest dreq;
         gazebo_msgs::DeleteModelResponse dresp;
@@ -279,13 +221,13 @@ void generate_path_RRT() {
   rrtTree tree;
   for (int i = 0; i < waypoints.size() - 1; i++) {
     if (i == 0) {
-      tree = rrtTree(waypoints.at(i), waypoints.at(i + 1), map,
-      map_origin_x, map_origin_y, res, margin);
+      tree = rrtTree(waypoints.at(i), waypoints.at(i + 1), map, map_origin_x,
+                     map_origin_y, res, margin);
     } else {
-      tree = rrtTree(path_RRT.back(), waypoints.at(i + 1), map,
-      map_origin_x, map_origin_y, res, margin);
+      tree = rrtTree(path_RRT.back(), waypoints.at(i + 1), map, map_origin_x,
+                     map_origin_y, res, margin);
     }
-    
+
     printf("New rrtTree generated.\n");
 
     std::vector<traj> path_tmp = tree.generateRRT(
@@ -323,14 +265,51 @@ void set_waypoints() {
   }
 }
 
-void callback_state(gazebo_msgs::ModelStatesConstPtr msgs) {
-  for (int i = 0; i < msgs->name.size(); i++) {
-    if (std::strcmp(msgs->name[i].c_str(), "racecar") == 0) {
-      robot_pose.x = msgs->pose[i].position.x;
-      robot_pose.y = msgs->pose[i].position.y;
-      robot_pose.th = tf::getYaw(msgs->pose[i].orientation);
+void set_waypoints() {
+  std::srand(std::time(NULL));
+  point waypoint_candid[5];
+  waypoint_candid[0].x = -3.5;
+  waypoint_candid[0].y = 12.0;
+
+  cv::Mat map_margin = map.clone();
+  int jSize = map.cols; // the number of columns
+  int iSize = map.rows; // the number of rows
+
+  for (int i = 0; i < iSize; i++) {
+    for (int j = 0; j < jSize; j++) {
+      if (map.at<uchar>(i, j) < 125) {
+        for (int k = i - waypoint_margin; k <= i + waypoint_margin; k++) {
+          for (int l = j - waypoint_margin; l <= j + waypoint_margin; l++) {
+            if (k >= 0 && l >= 0 && k < iSize && l < jSize) {
+              map_margin.at<uchar>(k, l) = 0;
+            }
+          }
+        }
+      }
     }
   }
+
+  // TODO 2
+  // Make your own code to select waypoints.
+  // You can randomly sample some points from the map.
+  // Also, the car should follow the track in clockwise.
+
+  waypoint_candid[4].x = -3.5;
+  waypoint_candid[4].y = 12.0;
+
+  int order[] = {0, 1, 2, 3, 4};
+  int order_size = 5;
+
+  for (int i = 0; i < order_size; i++) {
+    waypoints.push_back(waypoint_candid[order[i]]);
+  }
+}
+
+void callback_state(geometry_msgs::PoseWithCovarianceStampedConstPtr msgs) {
+  robot_pose.x = msgs->pose.pose.position.x;
+  robot_pose.y = msgs->pose.pose.position.y;
+  robot_pose.th = tf::getYaw(msgs->pose.pose.orientation);
+  // printf("x,y : %f,%f \n",robot_pose.x,robot_pose.y);
 }
 
 void setcmdvel(double v, double w) {
