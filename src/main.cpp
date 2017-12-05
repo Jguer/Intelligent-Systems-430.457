696c863a95529db2a832da3c4ed7f23c788ec1f0
Jguer <me@jguer.space>
2017-12-05 17:55:34 +0900

    Sanitizing

--- src/main.cpp
+++ src/main.cpp
@@ -57,282 +57,287 @@ void set_waypoints();
 void generate_path_RRT();
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "slam_main");
-  ros::NodeHandle n;
+    ros::init(argc, argv, "slam_main");
+    ros::NodeHandle n;
 
-  // Initialize topics
-  ros::Publisher cmd_vel_pub =
-      n.advertise<ackermann_msgs::AckermannDriveStamped>(
-          "/vesc/high_level/ackermann_cmd_mux/input/nav_0", 1);
+    // Initialize topics
+    ros::Publisher cmd_vel_pub =
+        n.advertise<ackermann_msgs::AckermannDriveStamped>(
+            "/vesc/high_level/ackermann_cmd_mux/input/nav_0", 1);
 
-  ros::Subscriber gazebo_pose_sub =
-      n.subscribe("/amcl_pose", 100, callback_state);
-  std::cout << "Initialized Topics" << std::endl;
+    ros::Subscriber gazebo_pose_sub =
+        n.subscribe("/amcl_pose", 100, callback_state);
+    std::cout << "Initialized Topics" << std::endl;
 
-  // FSM
-  state = INIT;
-  bool running = true;
-  PID *pid_ctrl = new PID(0.6, 0.3, 0.0);
-  ros::Rate control_rate(60);
-  int look_ahead_idx;
+    // FSM
+    state = INIT;
+    bool running = true;
+    PID *pid_ctrl = new PID(0.6, 0.3, 0.0);
+    ros::Rate control_rate(60);
+    int look_ahead_idx;
 
-  while (running) {
-    switch (state) {
-    case INIT: {
-      srand(time(NULL));
+    while (running) {
+        switch (state) {
+        case INIT: {
+            srand(time(NULL));
 
-      // Load Map
-      char *user = getpwuid(getuid())->pw_name;
-      cv::Mat map_org =
-          cv::imread((std::string("/home/") + std::string(user) +
-                      std::string("/catkin_ws/src/project4/src/slam_map.pgm"))
-                         .c_str(),
-                     CV_LOAD_IMAGE_GRAYSCALE);
+            // Load Map
+            char *user = getpwuid(getuid())->pw_name;
+            cv::Mat map_org =
+                cv::imread((std::string("/home/") + std::string(user) +
+                            std::string("/catkin_ws/src/project4/src/slam_map.pgm"))
+                           .c_str(),
+                           CV_LOAD_IMAGE_GRAYSCALE);
 
-      cv::transpose(map_org, map);
-      cv::flip(map, map, 1);
+            cv::transpose(map_org, map);
+            cv::flip(map, map, 1);
 
-      map_y_range = map.cols;
-      map_x_range = map.rows;
-      map_origin_x = map_x_range / 2.0 - 0.5;
-      map_origin_y = map_y_range / 2.0 - 0.5;
-      world_x_min = -4.5;
-      world_x_max = 4.5;
-      world_y_min = -13.5;
-      world_y_max = 13.5;
-      res = 0.05;
-      std::cout << "Loaded Map\n" << std::endl;
+            map_y_range = map.cols;
+            map_x_range = map.rows;
+            map_origin_x = map_x_range / 2.0 - 0.5;
+            map_origin_y = map_y_range / 2.0 - 0.5;
+            world_x_min = -4.5;
+            world_x_max = 4.5;
+            world_y_min = -13.5;
+            world_y_max = 13.5;
+            res = 0.05;
+            std::cout << "Loaded Map\n" << std::endl;
 
-      if (!map.data) {
-        // Check for invalid input
-        printf("Could not open or find the image\n");
-        return -1;
-      }
-      state = PATH_PLANNING;
-    } break;
-    case PATH_PLANNING: {
-      // Set Way Points
-      set_waypoints();
-      printf("Set way points\n");
-
-      // RRT
-      generate_path_RRT();
-      printf("Generate RRT\n");
-
-      ros::spinOnce();
-      ros::Rate(0.33).sleep();
-
-      printf("Initialize ROBOT\n");
-      state = RUNNING;
-    } break;
-    case RUNNING: {
-      if (path_RRT.size() == 0) {
-        printf("Path is empty.\n");
-        state = FINISH;
-        continue;
-      }
-
-      double dist_to_target = robot_pose.distance(path_RRT[look_ahead_idx].x,
-                                                  path_RRT[look_ahead_idx].y);
-      if (dist_to_target <= 0.4) {
-        std::cout << "New destination" << std::endl;
-        printf("x, y : %.2f, %.2f \n", path_RRT[look_ahead_idx].x,
-               path_RRT[look_ahead_idx].y);
-        /* Project: Ball Removal (crash)
-        gazebo_msgs::DeleteModelRequest dreq;
-        gazebo_msgs::DeleteModelResponse dresp;
-        dreq.model_name = std::to_string(look_ahead_idx);
-        deleteClient.call(dreq, dresp);
-        */
-        look_ahead_idx++;
-        if (look_ahead_idx == path_RRT.size()) {
-          std::cout << "Circuit Complete" << std::endl;
-          state = FINISH;
+            if (!map.data) {
+                // Check for invalid input
+                printf("Could not open or find the image\n");
+                return -1;
+            }
+            state = PATH_PLANNING;
         }
-      }
+        break;
+        case PATH_PLANNING: {
+            // Set Way Points
+            set_waypoints();
+            printf("Set way points\n");
 
-      speed =
-          2.0 - 1.2 / (1.0 + (robot_pose.distance(path_RRT[look_ahead_idx].x,
-                                                  path_RRT[look_ahead_idx].y)));
-      angle = pid_ctrl->get_control(robot_pose, path_RRT[look_ahead_idx]);
+            // RRT
+            generate_path_RRT();
+            printf("Generate RRT\n");
 
-      // Validate Speed
-      speed = (speed > max_speed) ? max_speed : speed;
-      speed = (speed < -max_speed) ? -max_speed : speed;
-      // Validate Angle
-      angle = (angle > max_turn) ? max_turn : angle;
-      angle = (angle < -max_turn) ? -max_turn : angle;
+            ros::spinOnce();
+            ros::Rate(0.33).sleep();
 
-      setcmdvel(speed, angle);
-      cmd_vel_pub.publish(cmd);
+            printf("Initialize ROBOT\n");
+            state = RUNNING;
+        }
+        break;
+        case RUNNING: {
+            if (path_RRT.size() == 0) {
+                printf("Path is empty.\n");
+                state = FINISH;
+                continue;
+            }
 
-      /* printf("Debug Parameters\n"); */
-      /* printf("Speed, Angle : %.2f, %.2f \n", speed, angle); */
-      /* printf("Car Pose : %.2f,%.2f,%.2f,%.2f,%.2f \n", robot_pose.x, */
-      /*        robot_pose.y, robot_pose.th, angle,
-       * path_RRT[look_ahead_idx].th); */
+            double dist_to_target = robot_pose.distance(path_RRT[look_ahead_idx].x,
+                                    path_RRT[look_ahead_idx].y);
+            if (dist_to_target <= 0.4) {
+                std::cout << "New destination" << std::endl;
+                printf("x, y : %.2f, %.2f \n", path_RRT[look_ahead_idx].x,
+                       path_RRT[look_ahead_idx].y);
+                /* Project: Ball Removal (crash)
+                gazebo_msgs::DeleteModelRequest dreq;
+                gazebo_msgs::DeleteModelResponse dresp;
+                dreq.model_name = std::to_string(look_ahead_idx);
+                deleteClient.call(dreq, dresp);
+                */
+                look_ahead_idx++;
+                if (look_ahead_idx == path_RRT.size()) {
+                    std::cout << "Circuit Complete" << std::endl;
+                    state = FINISH;
+                }
+            }
 
-      ros::spinOnce();
-      control_rate.sleep();
-    } break;
-    case FINISH: {
-      setcmdvel(0, 0);
-      cmd_vel_pub.publish(cmd);
-      running = false;
-      ros::spinOnce();
-      control_rate.sleep();
-    } break;
-    default: { } break; }
-  }
-  return 0;
+            speed =
+                2.0 - 1.2 / (1.0 + (robot_pose.distance(path_RRT[look_ahead_idx].x,
+                                    path_RRT[look_ahead_idx].y)));
+            angle = pid_ctrl->get_control(robot_pose, path_RRT[look_ahead_idx]);
+
+            // Validate Speed
+            speed = (speed > max_speed) ? max_speed : speed;
+            speed = (speed < -max_speed) ? -max_speed : speed;
+            // Validate Angle
+            angle = (angle > max_turn) ? max_turn : angle;
+            angle = (angle < -max_turn) ? -max_turn : angle;
+
+            setcmdvel(speed, angle);
+            cmd_vel_pub.publish(cmd);
+
+            /* printf("Debug Parameters\n"); */
+            /* printf("Speed, Angle : %.2f, %.2f \n", speed, angle); */
+            /* printf("Car Pose : %.2f,%.2f,%.2f,%.2f,%.2f \n", robot_pose.x, */
+            /*        robot_pose.y, robot_pose.th, angle,
+             * path_RRT[look_ahead_idx].th); */
+
+            ros::spinOnce();
+            control_rate.sleep();
+        }
+        break;
+        case FINISH: {
+            setcmdvel(0, 0);
+            cmd_vel_pub.publish(cmd);
+            running = false;
+            ros::spinOnce();
+            control_rate.sleep();
+        }
+        break;
+        default:
+        { } break;
+        }
+    }
+    return 0;
 }
 
 void generate_path_RRT() {
-  rrtTree tree;
-  for (int i = 0; i < waypoints.size() - 1; i++) {
-    if (i == 0) {
-      tree = rrtTree(waypoints.at(i), waypoints.at(i + 1), map, map_origin_x,
-                     map_origin_y, res, margin);
-    } else {
-      tree = rrtTree(path_RRT.back(), waypoints.at(i + 1), map, map_origin_x,
-                     map_origin_y, res, margin);
-    }
+    rrtTree tree;
+    for (int i = 0; i < waypoints.size() - 1; i++) {
+        if (i == 0) {
+            tree = rrtTree(waypoints.at(i), waypoints.at(i + 1), map, map_origin_x,
+                           map_origin_y, res, margin);
+        } else {
+            tree = rrtTree(path_RRT.back(), waypoints.at(i + 1), map, map_origin_x,
+                           map_origin_y, res, margin);
+        }
 
-    printf("New rrtTree generated.\n");
+        printf("New rrtTree generated.\n");
 
-    std::vector<traj> path_tmp = tree.generateRRT(
-        world_x_max, world_x_min, world_y_max, world_y_min, K, MaxStep);
-    printf("New trajectory generated.\n");
-    tree.visualizeTree(path_tmp);
+        std::vector<traj> path_tmp = tree.generateRRT(
+                                         world_x_max, world_x_min, world_y_max, world_y_min, K, MaxStep);
+        printf("New trajectory generated.\n");
+        tree.visualizeTree(path_tmp);
 
-    path_RRT.push_back(convertFromPoint(waypoints.at(i), 0.0, 0.0));
-    for (int k = 0; k < path_tmp.size(); k++) {
-      path_RRT.push_back(path_tmp[k]);
+        path_RRT.push_back(convertFromPoint(waypoints.at(i), 0.0, 0.0));
+        for (int k = 0; k < path_tmp.size(); k++) {
+            path_RRT.push_back(path_tmp[k]);
+        }
+        if (i == waypoints.size() - 2) {
+            path_RRT.push_back(convertFromPoint(waypoints.at(i + 1), 0.0, 0.0));
+        }
     }
-    if (i == waypoints.size() - 2) {
-      path_RRT.push_back(convertFromPoint(waypoints.at(i + 1), 0.0, 0.0));
-    }
-  }
 }
 
 void set_waypoints() {
-  std::srand(std::time(NULL));
-  point waypoint_candid[5];
-  waypoint_candid[0].x = -3.5;
-  waypoint_candid[0].y = 12.0;
+    std::srand(std::time(NULL));
+    point waypoint_candid[5];
+    waypoint_candid[0].x = -3.5;
+    waypoint_candid[0].y = 12.0;
 
-  cv::Mat map_margin = map.clone();
-  int jSize = map.cols; // the number of columns
-  int iSize = map.rows; // the number of rows
+    cv::Mat map_margin = map.clone();
+    int jSize = map.cols; // the number of columns
+    int iSize = map.rows; // the number of rows
 
-  for (int i = 0; i < iSize; i++) {
-    for (int j = 0; j < jSize; j++) {
-      if (map.at<uchar>(i, j) < 125) {
-        for (int k = i - waypoint_margin; k <= i + waypoint_margin; k++) {
-          for (int l = j - waypoint_margin; l <= j + waypoint_margin; l++) {
-            if (k >= 0 && l >= 0 && k < iSize && l < jSize) {
-              map_margin.at<uchar>(k, l) = 0;
+    for (int i = 0; i < iSize; i++) {
+        for (int j = 0; j < jSize; j++) {
+            if (map.at<uchar>(i, j) < 125) {
+                for (int k = i - waypoint_margin; k <= i + waypoint_margin; k++) {
+                    for (int l = j - waypoint_margin; l <= j + waypoint_margin; l++) {
+                        if (k >= 0 && l >= 0 && k < iSize && l < jSize) {
+                            map_margin.at<uchar>(k, l) = 0;
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
 
-  /*
-  map_y_range = map.cols;
-  map_x_range = map.rows;
-  map_origin_x = map_x_range / 2.0 - 0.5;
-  map_origin_y = map_y_range / 2.0 - 0.5;
-  world_x_min = -4.5;
-  world_x_max = 4.5;
-  world_y_min = -13.5;
-  world_y_max = 13.5;
-  res = 0.05;
-  int sector = 0;
-  */
+    /*
+    map_y_range = map.cols;
+    map_x_range = map.rows;
+    map_origin_x = map_x_range / 2.0 - 0.5;
+    map_origin_y = map_y_range / 2.0 - 0.5;
+    world_x_min = -4.5;
+    world_x_max = 4.5;
+    world_y_min = -13.5;
+    world_y_max = 13.5;
+    res = 0.05;
+    int sector = 0;
+    */
 
-  /* 
-  quadrants (<3 whitespace :*)
-  1   |   0
-  ____|____
-      | 
-  2   |   3
-  */
+    /*
+    quadrants (<3 whitespace :*)
+    1   |   0
+    ____|____
+        |
+    2   |   3
+    */
 
-  double quadrants[4][2] = { {world_x_max, world_y_max},
-                             {world_x_min, world_y_max},
-                             {world_x_min, world_y_min},
-                             {world_x_max, world_y_min} };
-  std::array<int, 3> quadrantSeq;
+    double quadrants[4][2] = {{world_x_max, world_y_max},
+        {world_x_min, world_y_max},
+        {world_x_min, world_y_min},
+        {world_x_max, world_y_min}
+    };
+    std::array<int, 3> quadrantSeq;
 
-
-  // check in which quadrant starting point is => generate sequence
-  if (waypoint_candid[0].y >= 0 && waypoint_candid[0].x >= 0) {
-    quadrantSeq = {3, 2, 1};
-    printf("Quadrant: %d \n", 0);
-  } else if (waypoint_candid[0].y >= 0 && waypoint_candid[0].x <= 0) {
-    quadrantSeq = {0, 3, 2};
-    printf("Quadrant: %d \n", 1);
-  } else if (waypoint_candid[0].y <= 0 && waypoint_candid[0].x <= 0) {
-    quadrantSeq = {1, 0, 3};
-    printf("Quadrant: %d \n", 2);
-  } else if (waypoint_candid[0].y <= 0 && waypoint_candid[0].x >= 0) {
-    quadrantSeq = {2, 1, 0};
-    printf("Quadrant: %d \n", 3);
-  } else {
-    printf("Quadrant exception, no waypoint creation possible!");
-  }
-
-  // find one Waypoint in each quadrant
-  bool foundPoint;
-  double x_rand, y_rand;
-  int i_rand, j_rand;
-  
-  for (int i = 0; i < quadrantSeq.size(); i++) {
-    foundPoint = false;
-    while (foundPoint == false) {
-      printf("Quadrant size: (x,y): %.2f, %.2f \n", quadrants[i][0], quadrants[i][1]);
-      x_rand = rand() / RAND_MAX * quadrants[i][0];
-      y_rand = rand() / RAND_MAX * quadrants[i][1];
-      i_rand = x_rand / res + map_origin_x;
-      j_rand = y_rand / res + map_origin_y;
-      printf("Random (x,y): %.2f, %.2f \n", x_rand, y_rand);
-
-      if ((map_margin.at<uchar>(i_rand, j_rand)) > 125) {
-        continue;
-      } else {
-        foundPoint = true;
-        waypoint_candid[i+1].x = x_rand;
-        waypoint_candid[i+1].y = y_rand;
-        printf("Waypoint found (x,y): %.2f, %.2f \n", 
-              waypoint_candid[i+1].x, waypoint_candid[i+1].y);
-        //optimization of position of point
-      }
+    // check in which quadrant starting point is => generate sequence
+    if (waypoint_candid[0].y >= 0 && waypoint_candid[0].x >= 0) {
+        quadrantSeq = {3, 2, 1};
+        printf("Quadrant: %d \n", 0);
+    } else if (waypoint_candid[0].y >= 0 && waypoint_candid[0].x <= 0) {
+        quadrantSeq = {0, 3, 2};
+        printf("Quadrant: %d \n", 1);
+    } else if (waypoint_candid[0].y <= 0 && waypoint_candid[0].x <= 0) {
+        quadrantSeq = {1, 0, 3};
+        printf("Quadrant: %d \n", 2);
+    } else if (waypoint_candid[0].y <= 0 && waypoint_candid[0].x >= 0) {
+        quadrantSeq = {2, 1, 0};
+        printf("Quadrant: %d \n", 3);
+    } else {
+        printf("Quadrant exception, no waypoint creation possible!");
     }
-  }
 
-  
+    // find one Waypoint in each quadrant
+    bool foundPoint;
+    double x_rand, y_rand;
+    int i_rand, j_rand;
 
-  waypoint_candid[4].x = -3.5;
-  waypoint_candid[4].y = 12.0;
+    for (int i = 0; i < quadrantSeq.size(); i++) {
+        foundPoint = false;
+        while (foundPoint == false) {
+            printf("Quadrant size: (x,y): %.2f, %.2f \n", quadrants[i][0],
+                   quadrants[i][1]);
+            x_rand = rand() / (RAND_MAX * quadrants[i][0]);
+            y_rand = rand() / (RAND_MAX * quadrants[i][1]);
+            i_rand = x_rand / res + map_origin_x;
+            j_rand = y_rand / res + map_origin_y;
+            printf("Random (x,y): %.2f, %.2f \n", x_rand, y_rand);
 
-  int order[] = {0, 1, 2, 3, 4};
-  int order_size = 5;
+            if ((map_margin.at<uchar>(i_rand, j_rand)) > 125) {
+                continue;
+            } else {
+                foundPoint = true;
+                waypoint_candid[i + 1].x = x_rand;
+                waypoint_candid[i + 1].y = y_rand;
+                printf("Waypoint found (x,y): %.2f, %.2f \n", waypoint_candid[i + 1].x,
+                       waypoint_candid[i + 1].y);
+                // optimization of position of point
+            }
+        }
+    }
 
-  for (int i = 0; i < order_size; i++) {
-    waypoints.push_back(waypoint_candid[order[i]]);
-  }
+    waypoint_candid[4].x = -3.5;
+    waypoint_candid[4].y = 12.0;
+
+    int order[] = {0, 1, 2, 3, 4};
+    int order_size = 5;
+
+    for (int i = 0; i < order_size; i++) {
+        waypoints.push_back(waypoint_candid[order[i]]);
+    }
 }
 
 void callback_state(geometry_msgs::PoseWithCovarianceStampedConstPtr msgs) {
-  robot_pose.x = msgs->pose.pose.position.x;
-  robot_pose.y = msgs->pose.pose.position.y;
-  robot_pose.th = tf::getYaw(msgs->pose.pose.orientation);
-  // printf("x,y : %f,%f \n",robot_pose.x,robot_pose.y);
+    robot_pose.x = msgs->pose.pose.position.x;
+    robot_pose.y = msgs->pose.pose.position.y;
+    robot_pose.th = tf::getYaw(msgs->pose.pose.orientation);
+    // printf("x,y : %f,%f \n",robot_pose.x,robot_pose.y);
 }
 
 void setcmdvel(double v, double w) {
-  cmd.drive.speed = v;
-  cmd.drive.steering_angle = w;
+    cmd.drive.speed = v;
+    cmd.drive.steering_angle = w;
 }
