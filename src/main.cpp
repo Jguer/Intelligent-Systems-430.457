914a9a8ce5a10f4eadd56d54c8f408dfe2bb60ef
Jguer <me@jguer.space>
2017-12-05 20:32:39 +0900

    Added thic margins

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ double world_y_min;
 double world_y_max;
 
 // parameters you should adjust : K, margin, MaxStep
-int margin = 15;
+int margin = 5;
 int K = 500;
 double MaxStep = 2;
 int waypoint_margin = 22;
